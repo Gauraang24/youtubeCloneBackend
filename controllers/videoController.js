0c9c48f5c84d0e1598e7e9df1d8621f0f84e5c2f
Gauraang24 <garryappdroid@gmail.com>
2024-12-15 17:40:16 +0530

    Videos Api: Commented Upload videos code, as its not part of the submission

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,64 +1,73 @@
 const Video = require("../models/videoModal"); // Import the Video model
-const cloudinary = require("cloudinary").v2;
+// const cloudinary = require("cloudinary").v2;
 
 // Get all videos
 exports.getVideos = async (req, res) => {
   try {
     const videos = await Video.find();
-    res.status(200).json(videos);
+    res.status(200).json({
+      data: videos,
+      status: true,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Failed to fetch videos", error: error.message });
+    res
+      .status(500)
+      .json({
+        message: "Failed to fetch videos",
+        error: error.message,
+        status: false,
+      });
   }
 };
 
 // Upload a new video
-exports.uploadVids = async (req, res) => {
-  try {
-    const { title, description } = req.body;
-    const videoFile = req.file;
+// exports.uploadVids = async (req, res) => {
+//   try {
+//     const { title, description } = req.body;
+//     const videoFile = req.file;
 
-    if (!videoFile) {
-      return res.status(400).json({ message: "No video file uploaded" });
-    }
+//     if (!videoFile) {
+//       return res.status(400).json({ message: "No video file uploaded" });
+//     }
 
-    // Upload video to Cloudinary
-    const uploadedVideo = await cloudinary.uploader.upload(videoFile.path, {
-      resource_type: "video",
-      folder: "youtube-clone-videos",
-    });
+//     // Upload video to Cloudinary
+//     const uploadedVideo = await cloudinary.uploader.upload(videoFile.path, {
+//       resource_type: "video",
+//       folder: "youtube-clone-videos",
+//     });
 
-    // Save video details to MongoDB
-    const newVideo = new Video({
-      title,
-      description,
-      videoUrl: uploadedVideo.secure_url,
-    });
+//     // Save video details to MongoDB
+//     const newVideo = new Video({
+//       title,
+//       description,
+//       videoUrl: uploadedVideo.secure_url,
+//     });
 
-    await newVideo.save();
-    res.status(201).json({ message: "Video uploaded successfully", video: newVideo });
-  } catch (error) {
-    res.status(500).json({ message: "Failed to upload video", error: error.message });
-  }
-};
+//     await newVideo.save();
+//     res.status(201).json({ message: "Video uploaded successfully", video: newVideo });
+//   } catch (error) {
+//     res.status(500).json({ message: "Failed to upload video", error: error.message });
+//   }
+// };
 
-// Remove a video
-exports.removeVideo = async (req, res) => {
-  try {
-    const { id } = req.params;
+// // Remove a video
+// exports.removeVideo = async (req, res) => {
+//   try {
+//     const { id } = req.params;
 
-    const video = await Video.findById(id);
-    if (!video) {
-      return res.status(404).json({ message: "Video not found" });
-    }
+//     const video = await Video.findById(id);
+//     if (!video) {
+//       return res.status(404).json({ message: "Video not found" });
+//     }
 
-    // Extract public_id from video URL to delete it from Cloudinary
-    const publicId = video.videoUrl.split("/").pop().split(".")[0];
-    await cloudinary.uploader.destroy(publicId, { resource_type: "video" });
+//     // Extract public_id from video URL to delete it from Cloudinary
+//     const publicId = video.videoUrl.split("/").pop().split(".")[0];
+//     await cloudinary.uploader.destroy(publicId, { resource_type: "video" });
 
-    await Video.findByIdAndDelete(id);
+//     await Video.findByIdAndDelete(id);
 
-    res.status(200).json({ message: "Video deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Failed to delete video", error: error.message });
-  }
-};
+//     res.status(200).json({ message: "Video deleted successfully" });
+//   } catch (error) {
+//     res.status(500).json({ message: "Failed to delete video", error: error.message });
+//   }
+// };
